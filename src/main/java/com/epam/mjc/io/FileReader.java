18e78c9e0a32675fc1077f7fb19973d983dbc175
fenix <fenixtheidragon@gmail.com>
2023-10-05 10:44:19 +0300

    Trying something with I/O streams

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException{
+        Profile profile;
+        FileInputStream fileInputStream = null;
+        //FileInputStream fileOutputStream = null;
+
+        try {
+            fileInputStream = new FileInputStream(file);
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                char ch = (char) c;
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        } finally {
+            if (fileInputStream != null) {
+                fileInputStream.close();
+
+            }
+        }
     }
 }
